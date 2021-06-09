0e45f09032e2e9e6a7e27cc57e7127c28961c124
Andreas Tomasini <andreas.tomasini@gmail.com>
2021-06-09 12:43:29 +0200

    Added a button do delete files, a button to add default sources if no sources are available and made the screen scrollable.

--- app/src/main/java/edu/tuk/satellitesarereal/UpdateScreen.kt
+++ app/src/main/java/edu/tuk/satellitesarereal/UpdateScreen.kt
@@ -1,85 +1,146 @@
 package edu.tuk.satellitesarereal
 
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import edu.tuk.satellitesarereal.ui.viewmodels.UpdateScreenViewModel
 
 
 @Composable
-fun UpdateScreen(updateScreenViewModel: UpdateScreenViewModel) {
-    val urls by updateScreenViewModel.urls.observeAsState()
+fun UpdateScreen(viewModel: UpdateScreenViewModel) {
+    val urls by viewModel.urls.observeAsState()
+    val fileList by viewModel.fileList.observeAsState()
+
     var inputText by remember { mutableStateOf("") }
 
+    val scrollState = ScrollState(0)
+
     Column(
-        Modifier.padding(horizontal = 6.dp)
+        modifier = Modifier
+            .padding(horizontal = 6.dp)
+            .verticalScroll(scrollState)
     ) {
-        Text("Update Screen")
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
+        UrlInput(inputText, viewModel) {
+            inputText = it
+        }
+        Spacer(Modifier.height(5.dp))
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { urls?.let { viewModel.onDownloadFiles(it) } }
         ) {
-            OutlinedTextField(
-                value = inputText,
-                onValueChange = { inputText = it },
-                label = { Text("URL") }
-            )
-            Button(
-                modifier = Modifier
-                    .align(Alignment.CenterVertically)
-                    .padding(start = 3.dp),
-                onClick = {
-                    updateScreenViewModel.onAddTleUrl(inputText)
-                    inputText = ""
+            Text(text = "DOWNLOAD FILES")
+        }
+
+        Spacer(Modifier.height(5.dp))
+
+        urls?.let {
+            if (it.isEmpty()) {
+                Button(
+                    modifier = Modifier.fillMaxWidth(),
+                    onClick = { viewModel.onLoadDefaultUrls() }
+                ) {
+                    Text("LOAD DEFAULT SOURCES")
                 }
-            ) {
-                Text(text = "Add url")
             }
         }
-        Divider(Modifier.padding(vertical = 4.dp))
-        urls?.forEach {
-            Card(
-                shape = RoundedCornerShape(8.dp),
-                border = BorderStroke(1.dp, Color.Gray),
-                elevation = 5.dp
-            ) {
-                Column {
-                    Text(
-                        text = it,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(top = 12.dp, start = 12.dp)
-                    )
-                    OutlinedButton(
-                        onClick = { updateScreenViewModel.onRemoveTleUrl(it) },
-                        border = BorderStroke(1.dp, MaterialTheme.colors.primary),
-                        modifier = Modifier
-                            .align(Alignment.End)
-                            .padding(bottom = 5.dp, end = 5.dp),
-                    ) {
-                        Text(stringResource(R.string.remove_item_link))
-                    }
-//                    Text(
-//                        text = stringResource(id = R.string.remove_item_link),
-//                        modifier = Modifier
-//                            .align(alignment = Alignment.End)
-//                            .padding(8.dp)
-//                            .clickable { someViewModel.onRemoveTleUrl(it) }
-//                            .border(width = 1.dp, Color.Blue, shape = RoundedCornerShape(3.dp))
-//                            .padding(horizontal = 5.dp, vertical = 2.dp),
-//                        color = Color.Blue
-//                    )
+
+        UrlCardList(urls, viewModel)
+
+        Spacer(Modifier.height(5.dp))
+
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = { fileList?.forEach { viewModel.onDeleteFile(it) } }
+        ) {
+            Text(text = "DELETE FILES")
+        }
+
+        fileList?.forEach { Text(it) }
+    }
+}
+
+@Composable
+private fun UrlCardList(
+    urls: List<String>?,
+    updateScreenViewModel: UpdateScreenViewModel,
+) {
+    urls?.forEach {
+        Card(
+            shape = RoundedCornerShape(8.dp),
+            border = BorderStroke(1.dp, Color.Gray),
+            elevation = 5.dp
+        ) {
+            Column {
+                Text(
+                    text = it,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 12.dp, start = 12.dp)
+                )
+                OutlinedButton(
+                    onClick = { updateScreenViewModel.onRemoveTleUrl(it) },
+                    border = BorderStroke(1.dp, MaterialTheme.colors.primary),
+                    modifier = Modifier
+                        .align(Alignment.End)
+                        .padding(bottom = 5.dp, end = 5.dp),
+                ) {
+                    Text(stringResource(R.string.remove_item_link))
                 }
             }
-            Spacer(modifier = Modifier.height(6.dp))
+        }
+        Spacer(modifier = Modifier.height(6.dp))
+    }
+}
+
+@Composable
+private fun UrlInput(
+    inputText: String,
+    updateScreenViewModel: UpdateScreenViewModel,
+    onInputTextUpdate: (value: String) -> Unit,
+) {
+    val context = LocalContext.current
+    val invalidUrlGiven = stringResource(R.string.invalid_url_input)
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            value = inputText,
+            onValueChange = { onInputTextUpdate(it) },
+            label = { Text("URL") }
+        )
+        Button(
+            modifier = Modifier
+                .align(Alignment.CenterVertically)
+                .padding(start = 3.dp),
+            onClick = {
+                try {
+                    updateScreenViewModel.onAddTleUrl(inputText)
+                } catch (e: UpdateScreenViewModel.MalformedUrlException) {
+                    Toast.makeText(
+                        context,
+                        invalidUrlGiven,
+                        Toast.LENGTH_LONG
+                    )
+                        .show()
+                }
+                onInputTextUpdate("")
+            }
+        ) {
+            Text(text = "Add url")
         }
     }
 }
--- app/src/main/java/edu/tuk/satellitesarereal/ui/viewmodels/UpdateScreenViewModel.kt
+++ app/src/main/java/edu/tuk/satellitesarereal/ui/viewmodels/UpdateScreenViewModel.kt
@@ -1,61 +1,85 @@
 package edu.tuk.satellitesarereal.ui.viewmodels
 
 import android.util.Log
+import android.util.Patterns.WEB_URL
 import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import edu.tuk.satellitesarereal.repositories.AppSettingsRepository
+import edu.tuk.satellitesarereal.repositories.TleFilesRepository
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 import javax.inject.Inject
 
 @HiltViewModel
 class UpdateScreenViewModel @Inject constructor(
-    val appSettingsRepository: AppSettingsRepository
+    val appSettingsRepository: AppSettingsRepository,
+    val tleFilesRepository: TleFilesRepository,
 ) : ViewModel() {
 
     companion object {
         private const val TAG = "SatAr:SomeViewModel"
     }
 
+    class MalformedUrlException(message: String) : Exception(message)
+
     private var _urls: MutableLiveData<List<String>> = MutableLiveData()
     val urls: LiveData<List<String>> get() = _urls
+
+    private var _fileList: MutableLiveData<List<String>> = MutableLiveData()
+    val fileList: LiveData<List<String>> get() = _fileList
 
     init {
         viewModelScope.launch {
             readUrls()
         }
+        getFileList()
     }
 
 
     private suspend fun readUrls() {
         appSettingsRepository.tleUrls().collect { value ->
-            if (value.isEmpty()) {
-                _urls.postValue(defaultSources())
-                Log.d(TAG, "readUrls: postValue(${defaultSources()}) executed")
-            } else {
-                Log.d(TAG, "readUrls: postValue($value) executed")
-                _urls.postValue(value)
-            }
+            _urls.postValue(value)
         }
     }
 
-    private fun defaultSources(): List<String> {
-        return listOf(
-            "https://celestrak.com/NORAD/elements/active.txt",
-            "https://www.prismnet.com/~mmccants/tles/classfd.zip",
-            "https://amsat.org/tle/current/nasabare.txt",
-            "https://www.prismnet.com/~mmccants/tles/inttles.zip"
-        )
+    private fun getFileList() {
+        _fileList.postValue(tleFilesRepository.listFiles())
+    }
+
+    fun onLoadDefaultUrls() {
+        viewModelScope.launch {
+            appSettingsRepository.saveTLEUrls(
+                listOf(
+                    "https://celestrak.com/NORAD/elements/active.txt",
+                    "https://www.prismnet.com/~mmccants/tles/classfd.zip",
+                    "https://amsat.org/tle/current/nasabare.txt",
+                    "https://www.prismnet.com/~mmccants/tles/inttles.zip"
+                )
+            )
+        }
     }
 
     fun onAddTleUrl(url: String) {
-        viewModelScope.launch {
-            val newUrls = _urls.value
-                ?.toMutableList()
-                ?.apply { add(url) }
-                ?.distinct()
-            newUrls?.let {
-                appSettingsRepository.saveTLEUrls(newUrls)
+        if (!WEB_URL.matcher(url).matches()) {
+            throw MalformedUrlException(
+                "UpdateScreenViewModel::onAddTleUrl(): the given URL '$url' is not a valid URL."
+            )
+        }
+
+        Log.d(TAG, "onAddTleUrl(): Adding URL: $url, urls: ${_urls.value}")
+
+        val newUrls = _urls.value
+            ?.toMutableList()
+            ?.apply { add(url) }
+            ?.distinct()
+
+        Log.d(TAG, "onAddTleUrl(): newUrls: $newUrls")
+        runBlocking {
+            viewModelScope.launch {
+                newUrls?.let {
+                    appSettingsRepository.saveTLEUrls(newUrls)
+                }
             }
         }
     }
@@ -71,12 +95,34 @@ class UpdateScreenViewModel @Inject constructor(
         }
     }
 
-    class Factory(private val appSettingsRepository: AppSettingsRepository) :
+    fun onDownloadFiles(it: List<String>) {
+        it.forEach { downloadFile(it) }
+    }
+
+    private fun downloadFile(file: String) {
+        viewModelScope.launch {
+            tleFilesRepository.downloadTleFile(file)
+            getFileList()
+        }
+    }
+
+    fun onDeleteFile(file: String) {
+        viewModelScope.launch {
+            tleFilesRepository.deleteFile(file)
+            getFileList()
+        }
+    }
+
+    class Factory(
+        private val appSettingsRepository: AppSettingsRepository,
+        private val tleFilesRepository: TleFilesRepository,
+    ) :
         ViewModelProvider.Factory {
         override fun <T : ViewModel?> create(modelClass: Class<T>): T {
             @Suppress("UNCHECKED_CAST")
             return UpdateScreenViewModel(
-                appSettingsRepository = appSettingsRepository
+                appSettingsRepository = appSettingsRepository,
+                tleFilesRepository = tleFilesRepository
             ) as T
         }
     }
